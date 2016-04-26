6e42fb684e3c71967f3600b26f134cb1250385ae
Witold Pomazanka <mulaczos@gmail.com>
2016-04-26 13:54:05 +0200

    dziala dynamiczne odswiezanie uzytkownikow po kliknieciu add

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -1,29 +1,42 @@
+var counter=0;
+
 var app = angular.module("app", [ 'app.services' ]);
+app.controller('CustomersController', [
+		'$scope',
+		'$http',
+		'$rootScope',
+		'CustomersFactory',
+		function($scope, $http, $rootScope, CustomersFactory) {
+					$scope.customerData = CustomersFactory.getAll(),
+					counter++,
+					$scope.counter=counter;
+					
+			$rootScope.$on("akcja", function() {
+				$scope.customerData = CustomersFactory.getAll();
 
-app.controller('CustomersController', [ '$scope', '$http', 'CustomersFactory',
-		function($scope, $http, CustomersFactory) {
-
-			$scope.customerData = CustomersFactory.getAll();
+			}, true);
 
 		} ]);
 
-app.controller('FormSubmitController', [ '$scope', '$http', 'CustomersFactory',
-		function($scope, $http, CustomersFactory) {
-	
+app.controller('FormSubmitController', [
+		'$scope',
+		'$http',
+		'$rootScope',
+		'CustomersFactory',
+		function($scope, $http, $rootScope, CustomersFactory) {
+
 			$scope.submit = function() {
 				CustomersFactory.add({
-					name : {
-						firstName : $scope.name.firstName,
-						lastName : $scope.name.lastName
-					}
+					name : $scope.name
 				}, function(responseData, httpData) {
-					$scope.id=responseData.id,
-					$scope.firstName=responseData.name.firstName,
-					$scope.lastName=responseData.name.lastName;
-				} );
-				$scope.id="",
-				$scope.firstName="",
-				$scope.lastName="";
+					$scope.id = responseData.id,
+							$scope.firstName = responseData.name.firstName,
+							$scope.lastName = responseData.name.lastName,
+							
+							$rootScope.$broadcast("akcja", {
+								id : responseData.id
+							});
+				});
+				$scope.id = "", $scope.firstName = "", $scope.lastName = "";
 			};
-			
 		} ]);
